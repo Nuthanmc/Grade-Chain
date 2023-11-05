8bdf3a7102ef8316bf218626ddbcf7452983675e
Mrudul Patel <mrudulpatel04@gmail.com>
2023-11-05 19:25:11 +0530

    Add Footer component to Home page

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       {/* Hero */}
       <Hero />
       {/* Footer */}
-      <footer className="footer bg-transparent text-center text-neutral-content">
+      {/* <footer className="footer bg-transparent text-center text-neutral-content">
         <div className="text-black dark:text-white flex w-[95vw] items-center flex-col m-5">
           <p className="footer-title">&copy; All rights reserved {new Date().getFullYear()}</p>
           <br />
@@ -18,7 +19,8 @@ export default function Home() {
             Made with <span className="text-red-600">❤️</span> by TEAM LEO
           </p>
         </div>
-      </footer>
+      </footer> */}
+      <Footer />
     </>
   );
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "@/styles";
+import { footerVariants } from "@/utils/motion";
+
+const Footer = () => (
+  <motion.footer
+    variants={footerVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="footer-gradient" />
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+
+        <div className="flex items-center justify-between flex-wrap gap-4">
+          <h4 className="font-extrabold text-[24px] text-white">CERTI-BLOCK</h4>
+          <p className="font-normal text-[14px] text-white opacity-50">
+            Copyright © {new Date().getFullYear() - 1} -{" "}
+            {new Date().getFullYear()} Metaversus. All rights reserved.
+          </p>
+
+          <div className="flex gap-4">
+            Made with <span className="text-[#FF0000]">&nbsp;❤&nbsp;</span>by TEAM LEO
+          </div>
+        </div>
+      </div>
+    </div>
+  </motion.footer>
+);
+
+export default Footer;
