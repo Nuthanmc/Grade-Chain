d56e9abc44d97492dc2b794fadd4d922f41b4141
Mrudul Patel <mrudulpatel04@gmail.com>
2024-01-19 18:18:52 +0530

    working on pdf generation of certificate

--- app/view-certificate/[id]/page.js
+++ app/view-certificate/[id]/page.js
@@ -1,20 +1,25 @@
 "use client";
 import { CertificateABI, certificateContractAddress } from "@/constants";
 import { ethers } from "ethers";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import jsPDF from "jspdf";
+import QRCode from "qrcode";
 
 const ViewCertificate = () => {
+  const [loading, setLoading] = useState(true);
   const [certificate, setCertificate] = useState({
     id: "",
     name: "",
     issuerName: "",
     course: "",
-    creation_date: ""
+    creation_date: "",
   });
   const { id } = useParams();
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const provider = new ethers.providers.WebSocketProvider(
           "wss://sepolia.infura.io/ws/v3/0aa8c89265604a2684abddfb063e3b42"
@@ -43,71 +48,93 @@ const ViewCertificate = () => {
           name: certificates[1] + " " + certificates[2],
           issuerName: certificates[3],
           course: certificates[5],
-          creation_date: certificates[6]
+          creation_date: certificates[6],
         });
+        setLoading(false);
+
+        const doc = new jsPDF("l", "px", "a4", true);
+
+        doc.addImage(
+          "/certificate-2-resized.jpg",
+          0,
+          0,
+          doc.internal.pageSize.width,
+          doc.internal.pageSize.height
+        );
+        doc.setTextColor(1, 1, 1);
+        doc.setFontSize(48);
+        doc.text(
+          certificates[1] + " " + certificates[2],
+          doc.internal.pageSize.width / 2,
+          170,
+          {
+            align: "center",
+          }
+        );
+        doc.setFontSize(20);
+        doc.text(certificates[5], doc.internal.pageSize.width / 2, 228, {
+          align: "center",
+        });
+
+        doc.setFontSize(10);
+        doc.text(
+          "Scan QR Code to verify",
+          doc.internal.pageSize.width / 2,
+          390,
+          {
+            align: "center",
+          }
+        );
+        doc.text(id, doc.internal.pageSize.width / 2, 400, {
+          align: "center",
+        });
+
+        // QR Code
+        const url = await QRCode.toDataURL(
+          "https://certi-block-web3.vercel.app/view-certificate/" + certificates[0],
+          {
+            width: 200,
+            height: 200,
+            margin: 0.2,
+            rendererOpts: {
+              quality: 1,
+            },
+          }
+        ).then((url) => {
+          return url;
+        });
+
+        console.log(url);
+        doc.addImage(
+          url,
+          "PNG",
+          doc.internal.pageSize.width / 2 - 50,
+          doc.internal.pageSize.height / 2 + 50,
+          100,
+          100
+        );
+        doc.save("certificate.pdf");
       } catch (error) {
         console.error("Error fetching data from blockchain:", error.message);
         // Handle error as needed
       }
     };
-
     fetchData();
   }, [id]);
+
   return (
-    <div className="h-screen w-full m-5 flex items-center justify-center">
-      <div className="card bg-white h-full border w-10/12">
-        <div className="card-header m-5 flex justify-between">
-          <div className="card-title">
-            <h4 className="text-2xl font-semibold">Certificate</h4>
-          </div>
-        </div>
-        <div className="card-body">
-          <div className="flex justify-between">
-            <div className="flex flex-col">
-              <div className="flex flex-row">
-                <span className="text-sm text-black font-semibold">
-                  Certificate Id:&nbsp;
-                </span>
-                <span className="text-sm font-bold text-gray-500">
-                  {certificate?.id}
-                </span>
-              </div>
-              <div className="flex flex-row">
-                <span className="text-sm text-black font-semibold">
-                  Reciever&apos;s Name:&nbsp;
-                </span>
-                <span className="text-sm font-bold text-gray-500">
-                  {certificate?.name}
-                </span>
-              </div>
-              <div className="flex flex-row">
-                <span className="text-sm text-black font-semibold">
-                  Certificate Issuer&apos;s Name:&nbsp;
-                </span>
-                <span className="text-sm font-bold text-gray-500">
-                  {certificate?.issuerName}
-                </span>
-              </div>
-              <div className="flex flex-row">
-                <span className="text-sm text-black font-semibold">
-                  Course Name:&nbsp;
-                </span>
-                <span className="text-sm font-bold text-gray-500">
-                  {certificate?.course}
-                </span>
-              </div>
-              <div className="flex flex-row">
-                <span className="text-sm text-black font-semibold">
-                  Certificate Issued Date:&nbsp;
-                </span>
-                <span className="text-sm font-bold text-gray-500">
-                  {certificate?.creation_date}
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="h-screen w-screen overflow-x-auto flex items-center justify-center">
+      {!loading ? (
+        <>
+          <p className="text-3xl lg:text-5xl xl:text-6xl mb-3 ml-3  absolute text-white">
+            This is just for testing purposes
+          </p>
+        </>
+      ) : (
+        <p className="text-3xl lg:text-5xl xl:text-6xl mb-3 ml-3  absolute text-white">
+          Loading...
+        </p>
+      )}
     </div>
   );
 };
