07190fd3ba9c44711fcff08680f55564171c26c7
Mrudul Patel <mrudulpatel04@gmail.com>
2024-02-01 09:57:50 +0530

    changed x icon

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -6,8 +6,7 @@ import {
   Instagram,
   LocalPhoneOutlined,
   LocationOnOutlined,
-  MailOutlineOutlined,
-  Twitter,
+  MailOutlineOutlined
 } from "@mui/icons-material";
 import Link from "next/link";
 import db from "@/config/firebase";
@@ -62,7 +61,7 @@ const ContactUs = () => {
     // contact us section
     <motion.div
       initial="hidden"
-      whileInView="show"  
+      whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
       className="flex justify-center mt-12 items-center lg:h-screen"
       id="contactus"
@@ -105,7 +104,27 @@ const ContactUs = () => {
                   <Instagram />
                 </a>
                 <a href="">
-                  <Twitter />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="512"
+                    height="512"
+                    fill="none"
+                    viewBox="0 0 512 512"
+                    id="twitter"
+                  >
+                    <g clip-path="url(#clip0_84_15697)">
+                      <rect width="512" height="512" fill="#000" rx="60"></rect>
+                      <path
+                        fill="#fff"
+                        d="M355.904 100H408.832L293.2 232.16L429.232 412H322.72L239.296 302.928L143.84 412H90.8805L214.56 270.64L84.0645 100H193.28L268.688 199.696L355.904 100ZM337.328 380.32H366.656L177.344 130.016H145.872L337.328 380.32Z"
+                      ></path>
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_84_15697">
+                        <rect width="512" height="512" fill="#fff"></rect>
+                      </clipPath>
+                    </defs>
+                  </svg>
                 </a>
               </div>
             </div>
